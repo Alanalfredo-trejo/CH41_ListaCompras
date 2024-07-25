20f1a0b838df1e2a03987f3f666c68dd9eeb5025
Alan Alfredo Trejo Garcia <alanalfredo.trejo@gmail.com>
2024-07-25 13:05:25 -0600

    Se actualiza la fecha automaticamente

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ let txtNumber = document.getElementById("Number");
 let alertValidaciones = document.getElementById("alertValidaciones");
 let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
 
+let fecha = document.getElementById("fecha");
+
 let contadorProductos = document.getElementById("contadorProductos"); //90
 let productosTotal = document.getElementById("productosTotal"); //97
 let precioTotal = document.getElementById("precioTotal"); //103
@@ -155,4 +157,7 @@ window.addEventListener("load", function (event) {
   contadorProductos.innerText = contador;
   productosTotal.innerText = totalEnProductos;
   precioTotal.innerText = `$ ${costoTotal.toFixed(2)}`;
+
+  let now = new Date();
+  fecha.innerText = now.getFullYear();
 }); //window load
